2b3cedd6e49e8f4d32658337180473c167611845
mbiriri <mbiririjoyce6@gmail.com>
2025-05-19 11:51:26 +0300

    Update Greeter.java

--- challenge 1/Greeter.java
+++ challenge 1/Greeter.java
@@ -1,6 +1,6 @@
 public class Greeter {
     void greet(){
-        System.out.println("hi there");
+        System.out.println("hi there");//output
     }
 
     public static void main(String[]args){
